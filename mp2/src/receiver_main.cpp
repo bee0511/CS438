3bcf79b839b51b3e3d01e8f4e6e4646d33074ece
bee0511 <bee000092.cs10@nycu.edu.tw>
2025-02-24 20:25:01 -0600

    [mp2] receiver class + sender class

--- mp2/src/receiver_main.cpp
+++ mp2/src/receiver_main.cpp
@@ -16,56 +16,63 @@
 #include <unistd.h>
 #include <pthread.h>
 
+class ReliableReceiver {
+private:
+    unsigned short int myUDPport;
+    char* destinationFile;
+    int s;
+    int slen;
+    struct sockaddr_in si_me;
+    struct sockaddr_in si_other;
+public:
+    ReliableReceiver(unsigned short int myUDPport, char* destinationFile);
+    void reliablyReceive();
+};
 
-
-struct sockaddr_in si_me, si_other;
-int s, slen;
-
-void diep(char *s) {
-    perror(s);
-    exit(1);
+ReliableReceiver::ReliableReceiver(unsigned short int myUDPport, char* destinationFile) {
+    this->myUDPport = myUDPport;
+    this->destinationFile = destinationFile;
 }
 
-
-
-void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
-    
+void ReliableReceiver::reliablyReceive() {
     slen = sizeof (si_other);
 
-
-    if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-        diep("socket");
+    if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1){
+        perror("socket");
+        exit(1);
+    }
 
     memset((char *) &si_me, 0, sizeof (si_me));
     si_me.sin_family = AF_INET;
     si_me.sin_port = htons(myUDPport);
     si_me.sin_addr.s_addr = htonl(INADDR_ANY);
     printf("Now binding\n");
-    if (bind(s, (struct sockaddr*) &si_me, sizeof (si_me)) == -1)
-        diep("bind");
+    if (bind(s, (struct sockaddr*) &si_me, sizeof (si_me)) == -1){
+        perror("bind");
+        exit(1);
+    }
 
-
-	/* Now receive data and send acknowledgements */    
+    /* Now receive data and send acknowledgements */
 
     close(s);
-	printf("%s received.", destinationFile);
+    printf("%s received.", destinationFile);
     return;
 }
+
+
 
 /*
  * 
  */
 int main(int argc, char** argv) {
 
-    unsigned short int udpPort;
-
     if (argc != 3) {
         fprintf(stderr, "usage: %s UDP_port filename_to_write\n\n", argv[0]);
         exit(1);
     }
 
-    udpPort = (unsigned short int) atoi(argv[1]);
-
-    reliablyReceive(udpPort, argv[2]);
+    ReliableReceiver receiver((unsigned short int) atoi(argv[1]), argv[2]);
+    
+    receiver.reliablyReceive();
 }
 
--- mp2/src/sender_main.cpp
+++ mp2/src/sender_main.cpp
@@ -20,18 +20,74 @@
 #include <string.h>
 #include <sys/time.h>
 
-struct sockaddr_in si_other;
-int s, slen;
+#include <iostream>
+#include <queue>
+#include <cmath>
+#include <errno.h>
+#include <cstdio>
 
-void diep(char *s) {
-    perror(s);
-    exit(1);
+#define MSS 1024    // Maximum Segment Size
+#define TIMEOUT 500000 // Timeout in milliseconds
+using namespace std;
+
+struct Packet {
+    uint64_t seq;
+    uint64_t ack;
+    char data[MSS];
+    uint64_t len;
+    enum PacketType {DATA, ACK, FIN, FINACK} type;
+};
+
+class ReliableSender {
+private:
+    char* hostname;
+    unsigned short int hostUDPport;
+    char* filename;
+    unsigned long long int bytesToTransfer;
+    FILE *fp;
+    int sockfd;
+    int slen;
+    struct sockaddr_in si_other;
+
+    uint64_t num_packets;
+    uint64_t seq;
+    uint64_t num_sent;
+    uint64_t num_recv;
+    uint64_t dupACKcount;
+    enum State {SLOW_START, CONGESTION_AVOID, FAST_RECOVERY} state;
+    uint64_t cwnd;
+    uint64_t ssthresh;
+
+
+public:
+    ReliableSender(char* hostname, unsigned short int hostUDPport, char* filename, unsigned long long int bytesToTransfer);
+    void init();
+    void reliablyTransfer();
+
+};
+
+ReliableSender::ReliableSender(char* hostname, unsigned short int hostUDPport, char* filename, unsigned long long int bytesToTransfer) {
+    this->hostname = hostname;
+    this->hostUDPport = hostUDPport;
+    this->filename = filename;
+    this->bytesToTransfer = bytesToTransfer;
+
+    this->fp = NULL;
+    this->sockfd = 0;
+    this->slen = 0;
+
+    this->num_packets = ceil((double)bytesToTransfer / MSS);
+    this->seq = 0;
+    this->num_sent = 0;
+    this->num_recv = 0;
+    this->dupACKcount = 0;
+    this->state = SLOW_START;
+    this->cwnd = MSS;
+    this->ssthresh = 64 * MSS;  // 64KB
 }
 
-
-void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* filename, unsigned long long int bytesToTransfer) {
+void ReliableSender::init(){
     //Open the file
-    FILE *fp;
     fp = fopen(filename, "rb");
     if (fp == NULL) {
         printf("Could not open file to send.");
@@ -42,8 +98,10 @@ void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* file
 
     slen = sizeof (si_other);
 
-    if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-        diep("socket");
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1){
+        perror("socket");
+        exit(1);
+    }
 
     memset((char *) &si_other, 0, sizeof (si_other));
     si_other.sin_family = AF_INET;
@@ -53,33 +111,37 @@ void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* file
         exit(1);
     }
 
+    // Set timeout for the socket
+    // Ref: https://stackoverflow.com/questions/4181784/how-to-set-socket-timeout-in-c-when-making-multiple-connections
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = TIMEOUT;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+        perror("setsockopt failed");
+        exit(1);
+    }
 
-	/* Send data and receive acknowledgements on s*/
-
-    printf("Closing the socket\n");
-    close(s);
-    return;
 
 }
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+void ReliableSender::reliablyTransfer() {
+    init();
 
-    unsigned short int udpPort;
-    unsigned long long int numBytes;
+    printf("Closing the socket\n");
+    close(sockfd);
+    return;
+}
+
+int main(int argc, char** argv) {
 
     if (argc != 5) {
         fprintf(stderr, "usage: %s receiver_hostname receiver_port filename_to_xfer bytes_to_xfer\n\n", argv[0]);
         exit(1);
     }
-    udpPort = (unsigned short int) atoi(argv[2]);
-    numBytes = atoll(argv[4]);
 
+    ReliableSender sender(argv[1], (unsigned short int) atoi(argv[2]), argv[3], atoll(argv[4]));
 
-
-    reliablyTransfer(argv[1], udpPort, argv[3], numBytes);
+    sender.reliablyTransfer();
 
 
     return (EXIT_SUCCESS);
